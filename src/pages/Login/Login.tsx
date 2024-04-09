015ecf298cda0f39220060500b0770e1a5084187
erikdiniz <erikdinizbeserra@gmail.com>
2024-04-09 17:00:00 -0300

    feat: adicionar imagem de fundo na tela de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,12 @@
 import { ButtonLogin } from "./ButtonLogin";
 import { FormInput } from "./FormInput";
 import style from "./Login.module.css"
+import imagem from "../../assets/design-todo-list.png"
 
 export function Login() {
     return (
         <div className={style.container}>
-            <div>
+            <div className={style.login}>
                 <h1 className={style.heading}>Login</h1>
                 
                 <form action="submit">
@@ -17,6 +18,12 @@ export function Login() {
                 </form>
 
                 <p className={style.p}>Não tem uma conta? <a href="" className={style.a}>Crie uma.</a></p>
+            </div>
+
+            <div className={style.containerRight}>
+                <img src={imagem} alt="to do list" className={style.imagem}/>
+                
+                <p className={style.logo}>LOGO</p>
             </div>
         </div>
     )
